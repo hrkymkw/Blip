94ff14f28e5d021b1343ef0e1bcff0481be680d5
ssiko <StevenTSikorski@gmail.com>
2015-06-15 16:17:51 -0400

    Konami.js and bundling...

--- Blip/Blip.Web/Global.asax.cs
+++ Blip/Blip.Web/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace Blip.Web
 {
@@ -11,6 +12,7 @@ namespace Blip.Web
     {
         protected void Application_Start()
         {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
