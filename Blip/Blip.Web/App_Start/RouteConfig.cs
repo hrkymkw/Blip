4a726b6478cbbc64e41e48581d6286ad3d43593c
ssiko <StevenTSikorski@gmail.com>
2015-06-28 20:59:56 -0400

    /login will now point to account/login now. Same goes for /register.

--- Blip/Blip.Web/App_Start/RouteConfig.cs
+++ Blip/Blip.Web/App_Start/RouteConfig.cs
@@ -10,6 +10,18 @@ namespace Blip.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Login",
+                "login/",
+                new { controller = "Account", action = "Login" }
+            );
+
+            routes.MapRoute(
+                "Register",
+                "register/",
+                new { controller = "Account", action = "Register" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
