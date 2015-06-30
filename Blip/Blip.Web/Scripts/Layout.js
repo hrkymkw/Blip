325f4d6d844c61d7a70ec15bff6b91c11a2dd6c4
ssiko <StevenTSikorski@gmail.com>
2015-06-30 14:01:54 -0400

    Issue #12. Fixing customer code that was inside of Konami.js and placing inside of Layout.js.

---
+++ Blip/Blip.Web/Scripts/Layout.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(window).konami({
+        code: [38, 40, 37, 39, 66, 65],
+        cheat: function () {
+            alertify.confirm('Achievement Unlocked. Enter funkytown?', function (e) {
+                if (e) {
+                    $('#konamiCheat').show();
+                }
+                else {
+                }
+            });
+
+        }
+    });
+});
--- Blip/Blip.Web/Scripts/konami.js
+++ Blip/Blip.Web/Scripts/konami.js
@@ -52,19 +52,3 @@
     };
 
 }(jQuery));
-
-$(document).ready(function () {
-    $(window).konami({
-        code: [38, 40,  37, 39, 66, 65],
-        cheat: function () {
-            alertify.confirm('Achievement Unlocked. Enter funkytown?', function (e) {
-                if (e) {
-                    $('#konamiCheat').show();
-                }
-                else {
-                }
-            });
-
-        }
-    });
-});
