4268ea2590667466693e1742698612f3fcf18a01
Hiro <tamokuteki@gmail.com>
2015-07-11 13:54:43 -0400

    Issue #5: Included jquery.validate and jquery.validate.unobtrusive in the bundle
    For now, just included jquery.validate and jquery.validate.unobtrusive
    in the bundle so that the login validation happens on the client side.
    Should we manually implement jquery validation script for this login
    form without using the razor?

--- Blip/Blip.Web/App_Start/BundleConfig.cs
+++ Blip/Blip.Web/App_Start/BundleConfig.cs
@@ -13,7 +13,8 @@ namespace Blip.Web
 
             bundles.Add(new ScriptBundle("~/bundles/mainjs").Include(
              "~/Scripts/bootstrap.min.js", "~/Scripts/jquery-1.10.2.min.js", "~/Scripts/alertify/alertify.js",
-             "~/Scripts/modernizr-2.6.2.js", "~/Scripts/jquery-ui-1.11.4.min.js", "~/Scripts/konami.js", "~/Scripts/Layout.js"));
+             "~/Scripts/modernizr-2.6.2.js", "~/Scripts/jquery-ui-1.11.4.min.js", "~/Scripts/jquery.validate.min.js", "~/Scripts/jquery.validate.unobtrusive.min.js",
+             "~/Scripts/konami.js", "~/Scripts/Layout.js"));
 
             bundles.Add(new StyleBundle("~/bundles/maincss").Include("~/Content/Site.css",
             "~/Content/bootstrap.min.css", "~/Content/themes/base/jquery-ui.css",
